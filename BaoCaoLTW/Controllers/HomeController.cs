7eea636bd5170293a8021671da8b1cb87b9f231c
pmd2902 <pmdphan@gmail.com>
2021-05-27 20:09:11 +0700

    bai-web

---
+++ BaoCaoLTW/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BaoCaoLTW.Models;
+
+namespace BaoCaoLTW.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        ShopGiaydbContextDataContext data = new ShopGiaydbContextDataContext();
+
+        public List<SanPham> Laysanpham(int count)
+        {          
+            return data.SanPhams.Where(x => x.TinhTrang == true).Take(count).ToList();
+        }
+
+        public ActionResult Index()
+        {
+            var sanpham = Laysanpham(3);
+            return View(sanpham);
+        }
+
+        public ActionResult Detail(string id)
+        {
+            var sanpham = from s in data.SanPhams
+                          where s.MaSanPham == id
+                          select s;
+            return View(sanpham.Single());
+        }
+
+    }
+}
---
+++ BaoCaoLTW/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BaoCaoLTW.Startup))]
+namespace BaoCaoLTW
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
