41d6ea4d92bbddbd8ae09344a5bf96483d94da81
pmd2902 <pmdphan@gmail.com>
2021-05-28 23:03:32 +0700

    Xong Gio Hang

---
+++ BaoCaoLTW/Controllers/GioHangController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BaoCaoLTW.Models;
+
+namespace BaoCaoLTW.Controllers
+{
+    public class GioHangController : Controller
+    {
+        ShopGiaydbContextDataContext data = new ShopGiaydbContextDataContext();
+        // GET: GioHang
+        public List<GioHang> LayGioHang()
+        {
+            List<GioHang> lstGioHang = Session["GioHang"] as List<GioHang>;
+            if (lstGioHang == null)
+            {
+                lstGioHang = new List<GioHang>();
+                Session["GioHang"] = lstGioHang;
+            }
+            return lstGioHang;
+        }
+
+        public ActionResult ThemGioHang(string sMaSanPham, string strURL)
+        {
+            List<GioHang> lstGioHang = LayGioHang();
+            GioHang sanpham = lstGioHang.Find(n => n.sMaSanPham == sMaSanPham);
+            if (sanpham == null)
+            {
+                sanpham = new GioHang(sMaSanPham);
+                lstGioHang.Add(sanpham);
+                return RedirectToAction("GioHang", "GioHang");
+            }
+            else
+            {
+                sanpham.iSoLuong++;
+                return RedirectToAction("GioHang", "GioHang");
+            }
+        }
+
+        private int TongSoLuong()
+        {
+            int iTongSoLuong = 0;
+            List<GioHang> lstGioHang = Session["GioHang"] as List<GioHang>;
+            if (lstGioHang != null)
+            {
+                iTongSoLuong = lstGioHang.Sum(n => n.iSoLuong);
+            }
+            return iTongSoLuong;
+        }
+        private double TongTien()
+        {
+            double iTongTien = 0;
+            List<GioHang> lstGioHang = Session["GioHang"] as List<GioHang>;
+            if (lstGioHang != null)
+            {
+                iTongTien = lstGioHang.Sum(n => n.dThanhTien);
+            }
+            return iTongTien;
+        }
+
+        public ActionResult GioHang()
+        {
+            List<GioHang> lstGioHang = LayGioHang();
+            if (lstGioHang.Count == 0)
+            {
+                return RedirectToAction("GioHang", "GioHang");
+            }
+            ViewBag.TongSoLuong = TongSoLuong();
+            ViewBag.TongTien = TongTien();
+            return View(lstGioHang);
+        }
+
+        public ActionResult GioHangPartial()
+        {
+            ViewBag.TongSoLuong = TongSoLuong();
+            ViewBag.TongTien = TongTien();
+            return PartialView();
+        }
+
+        public ActionResult XoaGioHang(string sMaSP)
+        {
+            List<GioHang> lstGioHang = LayGioHang();
+            GioHang sanpham = lstGioHang.SingleOrDefault(n => n.sMaSanPham == sMaSP);
+            if (sanpham != null)
+            {
+                lstGioHang.Remove(sanpham);
+                return RedirectToAction("GioHang");
+            }
+            if (lstGioHang.Count == 0)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return RedirectToAction("GioHang");
+        }
+
+        public ActionResult CapNhatGioHang(string sMaSP, FormCollection f)
+        {
+            List<GioHang> lstGioHang = LayGioHang();
+            GioHang sanpham = lstGioHang.SingleOrDefault(n => n.sMaSanPham == sMaSP);
+            if (sanpham != null)
+            {
+                sanpham.iSoLuong = int.Parse(f["txtSoLuong"].ToString());
+            }
+            return RedirectToAction("GioHang");
+        }
+
+        public ActionResult XoaTatCaGioHang()
+        {
+            List<GioHang> lstGioHang = LayGioHang();
+            lstGioHang.Clear();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ BaoCaoLTW/Models/GioHang.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BaoCaoLTW.Models
+{
+    public class GioHang
+    {
+        ShopGiaydbContextDataContext data = new ShopGiaydbContextDataContext();
+        public string sMaSanPham { get; set; }
+        public string sTenSanPham { get; set; }
+        public string sHinhAnh { get; set; }
+        public int iGia { get; set; }
+        public int iSoLuong { get; set; }
+        public Double dThanhTien
+        {
+            get { return iSoLuong * iGia; }
+        }
+
+        public GioHang(string MaSanPham)
+        {
+            sMaSanPham = MaSanPham;
+            SanPham sp = data.SanPhams.Single(n => n.MaSanPham == sMaSanPham);
+            sTenSanPham = sp.TenSanPham;
+            sHinhAnh = sp.HinhAnh;
+            iGia = int.Parse(sp.Gia.ToString());
+            iSoLuong = 1;
+        }
+    }
+
+    
+}
